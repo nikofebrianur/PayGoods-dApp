f9fc7f419885df97a37fc138aa6f12441b8c5c80
nikofebrianur <nikofebrianur@gmail.com>
2023-05-19 21:34:40 +0800

    add algo desc

---
+++ contracts/PayGoods.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract PayGoods {
+
+// Define the Owner of the smart contract
+
+// Create Struct and Mapping for request, transaction, and name
+
+// Add a name to wallet address
+
+// Create a Request
+
+// Pay a Request
+
+// Get all requests sent to a User
+
+// Fet all historic transactions user has been apart of
+}
