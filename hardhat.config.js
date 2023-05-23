f92ee1ee254ed65ce3853f7302406c1dd910c98d
nikofebrianur <nikofebrianur@gmail.com>
2023-05-23 21:12:36 +0800

    hardhat config, need POLYGON_MUMBAI_URL

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-etherscan");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
 };
